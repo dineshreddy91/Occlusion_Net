aacad06189228d863cd61090cf4e4249b1d5671b
Your Name <you@example.com>
2019-06-26 14:52:31 -0400

    running traing and testing

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -ul demo/* -v True -cfg occlusion-net.yaml
+docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -ul demo/* -v True -cfg data/occlusion_net_test.yaml
 
