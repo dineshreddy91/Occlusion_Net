87538c8908d2f4ffc4cc69aa8b2bd2245003c884
Mengqing Jiang <jiangmengqing1121@gmail.com>
2019-06-27 19:31:08 +0000

    change in config read

--- train.sh
+++ train.sh
@@ -4,6 +4,6 @@ docker run -v $PWD:/code \
         -v $BASE/annotations:/code/datasets/carfusion/annotations \
         -v $BASE/train:/code/datasets/carfusion/train \
         -v $BASE/test:/code/datasets/carfusion/test \
-        --shm-size=32GB --runtime=nvidia -it occlusion_net 
-        #--shm-size=32GB --runtime=nvidia $1 python train_net.py
+        --shm-size=32GB --runtime=nvidia $1 python train_net.py
+        #--shm-size=32GB --runtime=nvidia -it occlusion_net 
 
