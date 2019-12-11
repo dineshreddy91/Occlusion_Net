8ca083713ca64d205b2094c844ada4314fd704b4
Your Name <you@example.com>
2019-12-10 19:13:25 -0500

    fix test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -ul demo/* -v True -cfg data/occlusion_net_test.yaml
+docker run -v $PWD:/code --shm-size=32GB --runtime=nvidia $1 python infer.py -ul $2 -v True -cfg data/occlusion_net_test.yaml
 
